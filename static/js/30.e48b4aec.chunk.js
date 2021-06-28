32ab6406f86142b4195ecea446d3e5038ab14dce
黎海冬 <haidong.li@dfactory.tech>
2021-06-28 11:30:58 +0800

    release alpaha

---
+++ static/js/30.e48b4aec.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[30],{719:function(n,a,t){},740:function(n,a,t){"use strict";t.r(a),t.d(a,"default",(function(){return c}));var e=t(665),i=(t(719),t(56));function c(n){var a=n.lang;return Object(i.jsx)(e.a,{lang:a})}}}]);
+//# sourceMappingURL=30.e48b4aec.chunk.js.map
---
+++ static/js/31.9c25b5f2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[31],{714:function(n,e,a){},736:function(n,e,a){"use strict";a.r(e),a.d(e,"default",(function(){return t}));var c=a(664),i=(a(714),a(56));function t(n){var e=n.lang;return Object(i.jsx)("div",{className:"home_box",children:Object(i.jsx)(c.a,{lang:e})})}}}]);
+//# sourceMappingURL=31.9c25b5f2.chunk.js.map
