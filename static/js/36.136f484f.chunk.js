b493fcecde603692abafaef04bbf5a7169bf41cf
lhd-dfactory <83153425+lhd-defactory@users.noreply.github.com>
2021-07-12 21:59:41 +0800

    release

---
+++ static/js/36.136f484f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[36],{706:function(n,a,t){},731:function(n,a,t){"use strict";t.r(a),t.d(a,"default",(function(){return c}));var e=t(647),i=(t(706),t(54));function c(n){var a=n.lang;return Object(i.jsx)(e.a,{lang:a})}}}]);
+//# sourceMappingURL=36.136f484f.chunk.js.map
