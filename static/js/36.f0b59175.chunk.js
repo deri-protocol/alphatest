ebd56d13fb5865067aeb9d228d22228b1cbe9a05
黎海冬 <haidong.li@dfactory.tech>
2021-07-22 13:56:43 +0800

    release app

---
+++ static/js/36.f0b59175.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[36],{727:function(n,a,t){},759:function(n,a,t){"use strict";t.r(a),t.d(a,"default",(function(){return c}));var e=t(678),i=(t(727),t(60));function c(n){var a=n.lang;return Object(i.jsx)(e.a,{lang:a})}}}]);
+//# sourceMappingURL=36.f0b59175.chunk.js.map
---
+++ static/js/39.6b23e580.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[39],{736:function(n,a,e){},766:function(n,a,e){"use strict";e.r(a),e.d(a,"default",(function(){return s}));var c=e(677),t=(e(736),e(644)),i=e(60);function s(n){var a=n.lang;return Object(i.jsxs)("div",{className:"home_box",children:[Object(i.jsx)(c.a,{lang:a}),Object(i.jsx)(t.a,{lang:a})]})}}}]);
+//# sourceMappingURL=39.6b23e580.chunk.js.map
