ba1eaf08cf1bc6760b413788ddfd410adb0691f0
黎海冬 <haidong.li@dfactory.tech>
2021-08-20 15:26:12 +0800

    release app

---
+++ static/js/20.f4ebbeee.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[20],{700:function(n,a,e){},701:function(n,a,e){},702:function(n,a,e){},734:function(n,a,e){"use strict";e.r(a);var c=e(633),t=(e(700),e(701),e(702),e(624)),i=e(16);a.default=function({lang:n}){return Object(i.jsxs)("div",{className:"trade-container",children:[Object(i.jsx)(t.a,{lang:n}),Object(i.jsx)("div",{className:"trade-body",children:Object(i.jsx)(c.a,{lang:n})})]})}}}]);
+//# sourceMappingURL=20.f4ebbeee.chunk.js.map
