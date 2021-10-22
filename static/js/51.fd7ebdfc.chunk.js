4644afbc44f0da2619062dc92e6d74d5661b7f8f
wenchao.huang <wenchao.huang@dfactory.tech>
2021-10-22 11:27:30 +0800

    release app

---
+++ static/js/51.fd7ebdfc.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[51],{1292:function(n,a,t){},1337:function(n,a,t){"use strict";t.r(a),t.d(a,"default",(function(){return c}));var e=t(1006),i=(t(1292),t(18));function c(n){var a=n.lang;return Object(i.jsx)(e.a,{lang:a})}}}]);
+//# sourceMappingURL=51.fd7ebdfc.chunk.js.map
---
+++ static/js/56.f5b17cdd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[56],{1321:function(n,a,t){},1356:function(n,a,t){"use strict";t.r(a),t.d(a,"default",(function(){return c}));var e=t(999),i=(t(1321),t(18));function c(n){var a=n.lang;return Object(i.jsx)(e.a,{lang:a})}}}]);
+//# sourceMappingURL=56.f5b17cdd.chunk.js.map
