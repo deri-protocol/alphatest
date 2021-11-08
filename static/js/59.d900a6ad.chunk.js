6b9b7d77108b4cf540610f0b9424c2fe7471a371
黎海冬 <haidong.li@dfactory.tech>
2021-11-08 11:02:31 +0800

    release app

---
+++ static/js/59.d900a6ad.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[59],{1398:function(n,e,a){},1448:function(n,e,a){"use strict";a.r(e),a.d(e,"default",(function(){return t}));a(11);var c=a(1083),i=(a(1398),a(32));function t(n){var e=n.lang;return Object(i.jsx)("div",{className:"home_box",children:Object(i.jsx)(c.a,{lang:e})})}}}]);
+//# sourceMappingURL=59.d900a6ad.chunk.js.map
