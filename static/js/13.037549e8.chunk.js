5470f29f3967a824e5d06aaa5f1ff7f03e31be22
黎海冬 <haidong.li@dfactory.tech>
2021-12-08 18:22:11 +0800

    release app

---
+++ static/js/13.037549e8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[13],{909:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return c}));var i=t(11);function c(){return Object(i.jsx)("div",{children:"Home Page"})}}}]);
+//# sourceMappingURL=13.037549e8.chunk.js.map
---
+++ static/js/14.a03bf214.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[14],{912:function(i,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return c}));var e=t(11);function c(){return Object(e.jsx)("div",{children:"info Detail"})}}}]);
+//# sourceMappingURL=14.a03bf214.chunk.js.map
